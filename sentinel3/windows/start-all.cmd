5bd402b95397035ddf647f06105733b1fb5fa87f
Demis Bellot <demis.bellot@gmail.com>
2015-08-18 02:58:47 -0400

    Use snake_case and re-order shutdown order

--- sentinel3/windows/start-all.cmd
+++ sentinel3/windows/start-all.cmd
@@ -1,23 +1,22 @@
 @echo off
-SET redis-server=..\..\bin\windows\x64\redis-server
-SET redis-cli=..\..\bin\windows\x64\redis-cli
+SET redis_server=..\..\bin\windows\x64\redis_server
+SET redis_sentinel=..\..\bin\windows\x64\redis_server --sentinel
+SET redis_cli=..\..\bin\windows\x64\redis_cli
 
-start %redis-server% server-6380\redis.conf
-start %redis-server% server-6380\sentinel.conf --sentinel
+start %redis_server% server-6380\redis.conf
+start %redis_sentinel% server-6380\sentinel.conf
 
-start %redis-server% server-6381\redis.conf
-start %redis-server% server-6381\sentinel.conf --sentinel
+start %redis_server% server-6381\redis.conf
+start %redis_sentinel% server-6381\sentinel.conf --sentinel
 
-start %redis-server% server-6382\redis.conf
-start %redis-server% server-6382\sentinel.conf --sentinel
+start %redis_server% server-6382\redis.conf
+start %redis_sentinel% server-6382\sentinel.conf --sentinel
 
 echo Press enter to see sentinel info on masters and slaves...
 pause
 
-%redis-cli% -p 26380 sentinel master mymaster 
-%redis-cli% -p 26381 sentinel slaves mymaster
+%redis_cli% -p 26380 sentinel master mymaster
+%redis_cli% -p 26381 sentinel slaves mymaster
 
 echo Press enter again to close this window
 pause
-
-
--- sentinel3/windows/stop-all.cmd
+++ sentinel3/windows/stop-all.cmd
@@ -1,8 +1,9 @@
-SET redis-cli=..\..\bin\windows\x64\redis-cli
+SET redis_cli=..\..\bin\windows\x64\redis_cli
 
-%redis-cli% -p 26380 SHUTDOWN NOSAVE
-%redis-cli% -p 26381 SHUTDOWN NOSAVE
-%redis-cli% -p 26382 SHUTDOWN NOSAVE
-%redis-cli% -p 6380 SHUTDOWN NOSAVE
-%redis-cli% -p 6381 SHUTDOWN NOSAVE
-%redis-cli% -p 6382 SHUTDOWN NOSAVE
+%redis_cli% -p 26382 SHUTDOWN NOSAVE
+%redis_cli% -p 26381 SHUTDOWN NOSAVE
+%redis_cli% -p 26380 SHUTDOWN NOSAVE
+
+%redis_cli% -p 6382 SHUTDOWN NOSAVE
+%redis_cli% -p 6381 SHUTDOWN NOSAVE
+%redis_cli% -p 6380 SHUTDOWN NOSAVE
